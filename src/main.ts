6fe09cb00ec2d699338a43dbdccde5c1f543e756
“Randy” <randy.vilches@outlook.es>
2025-08-29 13:14:17 -0400

    configuracion inicial

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('API Conecta talentos')
+    .setDescription(
+      'Esta api describe la plataformad e ofertas laborales para estudiantes',
+    )
+    .setVersion('1.0')
+    .addTag('app-conecta-talentos-api')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
