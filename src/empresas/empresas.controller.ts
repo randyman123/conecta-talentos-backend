e312b934f93a56ecfbfbec7d7dbb91843f4f0103
“Randy” <randy.vilches@outlook.es>
2025-09-02 02:54:44 -0400

    empresas.controller

--- src/empresas/empresas.controller.ts
+++ src/empresas/empresas.controller.ts
@@ -1,8 +1,6 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Delete } from '@nestjs/common';
 import { EmpresasService } from './empresas.service';
 import { CreateEmpresaDto } from './dto/create-empresa.dto';
-import { UpdateEmpresaDto } from './dto/update-empresa.dto';
-
 @Controller('empresas')
 export class EmpresasController {
   constructor(private readonly empresasService: EmpresasService) {}
@@ -20,11 +18,6 @@ export class EmpresasController {
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.empresasService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateEmpresaDto: UpdateEmpresaDto) {
-    return this.empresasService.update(+id, updateEmpresaDto);
   }
 
   @Delete(':id')
